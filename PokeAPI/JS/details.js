60b912319bf3189059112684a3bbba30f665d260
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-10-09 11:35:28 +0200

    fix-solucionando los errores que ha provocado el compañero carlos con ChatGPT

--- PokeAPI/JS/details.js
+++ PokeAPI/JS/details.js
@@ -28,15 +28,23 @@ $(document).ready(function () {
             }
             var template = `
             <h3 class="text-center">Detalles del pokémon:</h3>
-
-                <ul class="list-group mt-3">
-                    <li class="list-group-item mb-1 p-3 fs-6 stats" style="width: 100%;"><strong>Altura:</strong>
-                        ${resp.height} cm</li>
-                    <li class="list-group-item mb-1 p-3 fs-6 stats" style="width: 100%;"><strong>Categoria:
-                        </strong>${capitalizeFirstLetter(resp.species.name)}</li>
-                    <li class="list-group-item mb-1 p-3 fs-6 stats" style="width: 100%;"><strong>Peso:</strong>
-                        ${resp.weight} kg</li>
-                </ul>
+            <div class="card mx-auto caja-ppal my-4">
+                <div class="card-header text-center ">
+                    <h5 class="card-title mb-0">${capitalizeFirstLetter(resp.name)} Nº${pokemonId.padStart(4, '0')}</h5>
+                </div>
+                <div class="card-body text-center">
+                    <div>
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png"
+                            alt="Foto de ${resp.name}" class="img-fluid float-start fotoPokemon">
+                    </div>
+                    <ul class="list-group mt-3">
+                        <li class="list-group-item mb-1 p-3 fs-6 stats" style="width: 100%;"><strong>Altura:</strong>
+                            ${resp.height} cm</li>
+                        <li class="list-group-item mb-1 p-3 fs-6 stats" style="width: 100%;"><strong>Categoria:
+                            </strong>${capitalizeFirstLetter(resp.species.name)}</li>
+                        <li class="list-group-item mb-1 p-3 fs-6 stats" style="width: 100%;"><strong>Peso:</strong>
+                            ${resp.weight} kg</li>
+                    </ul>
                 <div class="container-fluid">
                     <h6 class="col-6 just mx-auto text-center">Ataques:</h6>
                     <div class="col-12 btn-group borde-habilidades" role="group" aria-label="Ataques">
